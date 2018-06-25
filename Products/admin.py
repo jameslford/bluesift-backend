36c7effb1de29726515e46938c5493683493a9ee
James Ford <jameslford8@gmail.com>
2018-06-25 19:02:55 -0400

    refined query methods for product.models

--- Products/admin.py
+++ Products/admin.py
@@ -2,7 +2,8 @@
 
 from django.contrib import admin
 
-from .models import Manufacturer, Product
+from .models import Manufacturer, Product, SupplierProduct
 
 admin.site.register(Manufacturer)
 admin.site.register(Product)
+admin.site.register(SupplierProduct)
--- Products/models.py
+++ Products/models.py
@@ -12,12 +12,26 @@ class Manufacturer(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    image = models.ImageField()
+    name            = models.CharField(max_length=200)
+    manufacturer    = models.ForeignKey(
+                        Manufacturer, 
+                        on_delete=models.CASCADE,
+                        )
+    image           = models.ImageField()
 
     def __str__(self):
-        return self.name 
+        return self.name
+
+    def is_priced(self):
+        if self.priced:
+            return True
+        return False
+
+    def get_suppliers(self):
+        if self.priced:
+            suppliers = self.priced.all()
+            return suppliers
+
 
 
 class SupplierProduct(models.Model):
@@ -28,10 +42,17 @@ class SupplierProduct(models.Model):
         (3, 'Each')
     )
 
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    supplier = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    price_per_unit = MoneyField(max_digits=8, decimal_places=2, default_currency='USD')
-    unit = models.SmallIntegerField(choices=UNITS)
+    product             = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='priced')
+    supplier            = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                                            on_delete=models.CASCADE, 
+                                            related_name='supplier_products',
+                                            limit_choices_to={'is_supplier' : True}
+                                            )
+    price_per_unit      = MoneyField(max_digits=8, decimal_places=2, default_currency='USD')
+    unit                = models.SmallIntegerField(choices=UNITS)
+
+    def __str__(self):
+        return self.supplier.get_first_name() + ' ' + self.product.name
 
     class Meta:
         unique_together = ('product','supplier')
