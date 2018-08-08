137c52a894a227e9392bb8bed41d8e8cb77fd88a
James Ford <jameslford8@gmail.com>
2018-08-08 19:59:53 -0400

    helping

--- Profiles/views.py
+++ Profiles/views.py
@@ -111,14 +111,14 @@ def get_project_products(project):
         content = {
             'use': product.use,
             'name': product.product.name,
-            # 'image': product.product.image,
+            'image': str(product.product.image),
             'product_id': product.product.id,
             'id': product.id,
             # 'prices': product.product.prices(),
             'lowest_price': product.product.lowest_price,
             'is_priced': product.product.is_priced,
             'product_type': product.product.product_type.material,
-            # 'manufacturer_url': product.product.manufacturer_url
+            'manufacturer_url': str(product.product.manufacturer_url)
         }
         product_list.append(content)
     return product_list
