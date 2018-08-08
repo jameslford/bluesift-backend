a736c51b81b1ef2666f7f2bdd138dcaab797bc4f
James Ford <jameslford8@gmail.com>
2018-08-08 16:56:34 -0400

    wombat

--- Profiles/admin.py
+++ Profiles/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import CompanyAccount, CompanyShippingLocation, CustomerProfile, CustomerProject
+from .models import(
+                    CompanyAccount, 
+                    CompanyShippingLocation, 
+                    CustomerProfile,
+                    CustomerProduct, 
+                    CustomerProject,
+                    SupplierProduct
+                    )
 
 
 admin.site.register(CompanyAccount)
 admin.site.register(CompanyShippingLocation)
 admin.site.register(CustomerProfile)
 admin.site.register(CustomerProject)
+admin.site.register(SupplierProduct)
+admin.site.register(CustomerProduct)
+
 # Register your models here.
