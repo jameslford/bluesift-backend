7c3e2e6148f0bc789853b310414de290bdc22cb2
James Ford <jameslford8@gmail.com>
2018-08-23 22:51:44 -0400

    kamala

--- Products/views.py
+++ Products/views.py
@@ -131,13 +131,13 @@ def product_list(request):
     product_count = products.count()
 
     filter_response = {
+        'for_sale': bool_response(products, product_boolean_args[0]),
+        'filter_bools': bool_response(products, product_boolean_args[1:]),
         'manufacturers' : filter_manufacturer,
-        'all_cats': all_cats,
-        'filter_bools': bool_response(products, product_boolean_args[1:])
+        'all_cats': all_cats
     }
 
     return Response({
-        'for_sale': for_sale[0],
         'product_count': product_count,
         'filter': filter_response,
         'products': ProductSerializer(products, many=True).data
