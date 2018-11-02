bf96ef5d1a7b20e06130a97ce374dc894e8dc977
James Ford <jameslford8@gmail.com>
2018-11-02 13:03:13 -0400

    maggie

--- Addresses/models.py
+++ Addresses/models.py
@@ -5,7 +5,7 @@ class Address(models.Model):
     address_line_1 = models.CharField(max_length=120)
     address_line_2 = models.CharField(max_length=120, null=True, blank=True)
     city = models.CharField(max_length=120)
-    country = models.CharField(max_length=120, default='United States of America')
+    country = models.CharField(max_length=120, blank=True, null=True, default='United States of America')
     state = models.CharField(max_length=120, choices=states)
     postal_code = models.CharField(max_length=11)
 
