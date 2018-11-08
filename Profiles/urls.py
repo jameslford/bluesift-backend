d4091b04539f9c4c69d217cb91b508aadc2dbd4e
James Ford <jameslford8@gmail.com>
2018-11-07 23:54:26 -0500

    bill

--- Profiles/urls.py
+++ Profiles/urls.py
@@ -6,7 +6,7 @@ from .views import(
     append_lib,
     supplier_list,
     supplier_detail,
-    add_supplier_location,
+    supplier_location,
     supplier_product
     )
 
@@ -16,6 +16,6 @@ urlpatterns = [
     path('appendLib', append_lib),
     path('supplierList', supplier_list),
     path('supplierDetails/<int:pk>', supplier_detail),
-    path('addSupplierLocation', add_supplier_location),
+    path('supplierLocation', supplier_location),
     path('supplierProduct/<int:pk>/', supplier_product )
 ]
