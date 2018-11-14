31f4a18d7710e64e5fba4ceccf48dd6e58059fda
James Ford <jameslford8@gmail.com>
2018-11-13 22:31:15 -0500

    onandon

--- Profiles/models.py
+++ Profiles/models.py
@@ -130,7 +130,7 @@ class CustomerProfile(models.Model):
         on_delete=models.CASCADE,
         related_name='user_profile'
         )
-    addresses = models.ManyToManyField(Address, related_name='addresses')
+    addresses = models.ManyToManyField(Address, null=True, related_name='addresses')
     phone_number = models.IntegerField(null=True)
     name = models.CharField(max_length=40, blank=True, null=True)
 
