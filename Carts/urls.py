6c836492e20dc6f3b34a95865f8ce3465ef4db1a
James Ford <jameslford8@gmail.com>
2018-11-19 19:15:16 -0500

    murphy

--- Carts/urls.py
+++ Carts/urls.py
@@ -4,5 +4,5 @@ from .views import add_to_cart, cart_details
 
 urlpatterns = [
     path('addtocart/', add_to_cart),
-    path('details/', cart_details),
+    path('details/<int:pk>', cart_details),
 ]
