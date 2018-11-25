aa7ae7dc90e755f649d851a62dc739cc389b8022
James Ford <jameslford8@gmail.com>
2018-11-25 14:32:32 -0500

    tony

--- Accounts/views.py
+++ Accounts/views.py
@@ -50,8 +50,7 @@ def create_user(request):
     email = request.data.get('email')
     password = request.data.get('password')
     company_name = request.data.get('company_account')['company_name']
-    phone_number_str = request.data.get('company_account')['phone_number']
-    phone_number = int(phone_number_str)
+    phone_number = request.data.get('company_account')['phone_number']
     user_model = get_user_model()
     user = None
 
