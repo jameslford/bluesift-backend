4cb08877832a788f5ab3c0eab8222c012a4e1da5
James Ford <jameslford8@gmail.com>
2018-11-29 14:09:44 -0500

    hol

--- Accounts/views.py
+++ Accounts/views.py
@@ -135,7 +135,7 @@ def custom_login(request):
         return Response('Invalid Credentials', status=status.HTTP_404_NOT_FOUND)
 
     if not check_password(password, user.password):
-        return Response('Invalid Credentials', status=status.HTTP_404_NOT_FOUND)
+        return Response('Invalid Credentials password', status=status.HTTP_404_NOT_FOUND)
 
     if not user.is_active:
         return Response(
