46675d31494c79dd2cc95cd45e80d4e891f4dcb1
James Ford <jameslford8@gmail.com>
2019-02-05 12:50:54 -0500

    replaced prefetch related with prefetch related objects

--- Profiles/views.py
+++ Profiles/views.py
@@ -100,7 +100,7 @@ def sv_supplier_location(request, pk=None):
         if not location:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         serialized = SVLocationSerializer(
-            location.prefetch_related('priced_products'),
+            location.select_related('priced_products'),
             context={
                 'order_by': order_by,
                 'search': search_request
