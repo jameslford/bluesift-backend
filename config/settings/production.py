6942ff8a8495705194c39b89198e1fcaf97a1ad6
James Ford <jameslford8@gmail.com>
2019-02-28 20:02:20 -0500

    last commit before deploying

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,7 @@ import os
 import dj_database_url
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 
 STATIC_URL = '/static/'
@@ -16,8 +16,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 REDIRECT_URL = 'https://www.google.com/'
 DEFAULT_ADDRESS_INSTANCE = 1
 
-AWS_STATIC_BUCKET_NAME = 'pixidesk-staging'
-AWS_MEDIA_BUCKET_NAME = "pixidesk-staging-media"
+AWS_STATIC_BUCKET_NAME = 'pixidesk-production'
+AWS_MEDIA_BUCKET_NAME = "pixidesk-production-media"
 AWS_S3_REGION_NAME = 'us-east-1'
 
 
@@ -62,7 +62,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 DATA_PATH = PRODUCTION_DATA_WRITE_PATH
-ZIP_PATH =  os.getcwd() + '/config/management/zips/zipcodes.csv'
+ZIP_PATH = os.getcwd() + '/config/management/zips/zipcodes.csv'
 
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
@@ -95,3 +95,16 @@ MIDDLEWARE = [
 
 # 'GDAL_LIBRAY_PATH' '/app/.heroku/vendor/lib/libgdal.so'
 # 'GEOS_LIBRARY_PATH' '/app/.heroku/vendor/lib/libgeos_c.so'
+
+
+# AWS_ACCESS_KEY_ID :    AKIAJMXZBZGT7YLT2QCA
+# AWS_SECRET_ACCESS_KEY : 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
+# BUILD_WITH_GEO_LIBRARIES : 1
+# DATABASE_URL : postgres://jhawnscgwbzplm:8229cdbbf4e81a3b3875131f3ca2912b11b441eb90690e357c95f0ed9fb37b56@ec2-54-225-237-84.compute-1.amazonaws.com:5432/de1p8mp8pa6hj5
+# DISABLE_COLLECTSTATIC : 1
+# DJANGO_SETTINGS_MODULE : config.settings.production
+# GDAL_LIBRAY_PATH : /app/.heroku/vendor/lib/libgdal.so
+# GEOS_LIBRARY_PATH : /app/.heroku/vendor/lib/libgeos_c.so
+# SECRET_KEY : 7_gs57^=4i^3l74e=(*ko2m4&-ws82o3akofa6$+26a)sqqxa4
+
+# postgres://mzfomutrqqlhaj:dcef1fb2409b78a68ad4639da3d8be531aa0cc86d151f500ba440bd464fe710e@ec2-54-235-159-101.compute-1.amazonaws.com:5432/dl7aa1f30f3b8
