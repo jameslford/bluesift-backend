b8bb4bc24a887f977392259a80e29683fb072fd7
James Ford <jameslford8@gmail.com>
2019-05-27 14:30:45 -0500

    added return values to task hoping they'll log

--- config/tasks.py
+++ config/tasks.py
@@ -7,16 +7,22 @@ from config.scripts.db_operations import backup_db, clean_backups
 def check_cache():
     from ProductFilter.models import QueryIndex
     dirty_qis = QueryIndex.objects.filter(dirty=True)
+    dirty_count = dirty_qis.count()
+    cleaned = 0
     for index in dirty_qis:
         if QueryIndex.objects.filter(pk=index.pk).exists():
             index.refresh()
+            cleaned += 1
+    return f'dirty={dirty_count}, cleaned={cleaned}'
 
 
 @shared_task
 def backup_db_task():
     backup_db()
+    return 'backup complete'
 
 
 @shared_task
 def clean_backups_task():
     clean_backups()
+    return 'backups_cleaned'
