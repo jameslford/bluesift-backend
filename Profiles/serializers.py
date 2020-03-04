b2dc1ee6fa9dea703fe6c6e466a4db2dd38a0150
jlford5 <jford@bluesift.com>
2020-03-04 00:07:53 -0500

    typo

--- Profiles/serializers.py
+++ Profiles/serializers.py
@@ -13,7 +13,7 @@ def employee_mini_serializer(profile: SupplierEmployeeProfile):
 def base_mini_serializer(profile: BaseProfile):
     return {
         'pk': profile.pk,
-        'name': profile.user.get_ful_name(),
+        'name': profile.user.get_full_name(),
         'email': profile.user.email(),
         'avatar': profile.avatar,
         }
