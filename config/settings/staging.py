6704d1f42bf359ad26a0e25380d38f93e2459450
jlford5 <jford@bluesift.com>
2020-04-26 16:52:17 -0400

    updated middleware setting staging

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -110,6 +110,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'config.custom_middleware.StagingMiddleware',
-    # 'config.custom_middleware.LastSeenMiddleware',
+    'config.custom_middleware.LastSeenMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
