a8a45798ecf6af2856fa1fce76e127119d4a2c71
jlford5 <jford@bluesift.com>
2020-04-28 19:08:18 -0400

    add-demo

--- config/management/commands/ops_add_fake.py
+++ config/management/commands/ops_add_fake.py
@@ -3,12 +3,21 @@ from django.db import transaction
 from django.contrib.auth import get_user_model
 from Suppliers.models import SupplierCompany
 from Profiles.models import SupplierEmployeeProfile
-from scripts.demo_data import create_demo_users, add_additonal, create_parent_tasks, create_child_tasks
+from scripts.demo_data import (
+    create_demo_users,
+    add_additonal,
+    create_parent_tasks,
+    create_child_tasks,
+)
 
 
 def delete_fake():
     user_model = get_user_model()
-    fake_comps = SupplierEmployeeProfile.objects.filter(user__demo=True).values_list('company__pk', flat=True).distinct()
+    fake_comps = (
+        SupplierEmployeeProfile.objects.filter(user__demo=True)
+        .values_list("company__pk", flat=True)
+        .distinct()
+    )
     for comp in fake_comps:
         company = SupplierCompany.objects.get(pk=comp)
         company.delete()
@@ -21,11 +30,11 @@ def replace():
     delete_fake()
     create_demo_users()
 
-class Command(BaseCommand):
 
+class Command(BaseCommand):
     def handle(self, *args, **options):
-        # replace()
-        # add_additonal()
-        
+        replace()
+        add_additonal()
+
         create_parent_tasks()
         create_child_tasks()
